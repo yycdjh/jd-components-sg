b0c8035d89b5dde498aff8e6395f3371aca3be08
JD <591883039@qq.com>
2024-09-29 09:39:25 +0800

    fix: change Onboarding

--- src/OnBoarding/Mark.tsx
+++ src/OnBoarding/Mark.tsx
@@ -42,10 +42,8 @@ export const Mask: React.FC<MaskProps> = (props) => {
   };
 
   return (
-    <>
-      <div style={style} className="mask">
-        {getContent()}
-      </div>
-    </>
+    <div style={style} className="mask">
+      {getContent()}
+    </div>
   );
 };
